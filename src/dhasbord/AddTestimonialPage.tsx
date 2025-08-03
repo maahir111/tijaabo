c41adaf028c5627e053631b52072ca980b46d985
maahir111 <daadira392@gmail.com>
2025-08-03 14:41:06 +0300

    update4/3

--- src/dhasbord/AddTestimonialPage.tsx
+++ src/dhasbord/AddTestimonialPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { API_BASE_URL } from "@/lib/constants";
+import { API_BASE_URL } from "./config";
 
 const AddTestimonialPage = () => {
   const navigate = useNavigate();
